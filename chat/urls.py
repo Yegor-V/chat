f80e4f321b8313a277e30a11449c6aa621dcc78e
Yegor <vasilchenko.yegor@gmail.com>
2016-09-14 21:15:31 +0300

    added urls for admin views

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from chat_app.views import RegistrationView, LoginView
+from chat_app.admin_view import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -10,4 +11,7 @@ urlpatterns = [
     url(r'^registration_handler$', RegistrationView.as_view()),
     url(r'^login$', LoginView.as_view()),
     url(r'^login_handler$', LoginView.as_view()),
+    url(r'^get_users$', get_all_users),
+    url(r'^add_user$', add_user_to_blacklist),
+    url(r'^delete_user/([\w]+)$', delete_user_from_blacklist),
 ]
